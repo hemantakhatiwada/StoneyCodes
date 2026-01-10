8b3d0b61e208a89d02aa0cc6bd2aa0eae1436c20
Hemant Khatiwada <hemantkhatiwada27@gmail.com>
2026-01-10 09:17:11 +0545

    1365

---
+++ src/Numbersmallerthancurrentnumber1365.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        // Arrays.sort(nums);
+        
+        for (int i=0;i<nums.length;i++)
+        {
+            int count=0;//count yaa bhitra use garo bhane fresh count hunxa for every numbers.
+            for (int j=0;j<nums.length;j++)
+            {
+                if (nums[j]<nums[i])
+                {
+count=count+1;
+                }
+                
+            }
+             ans[i]=count;
+        }
+        return ans;
+        
+    }
+}
+
+
+public class Numbersmallerthancurrentnumber1365
+{
+    public static void main (String[] args)
+    {
+        Solution obj = new Solution();
+        int[] arr = { 2,3,8,9};
+        int[] res= obj.smallerNumbersThanCurrent(arr);
+       
+       System.out.println(Arrays.toString(res));
+
+    }
+}
